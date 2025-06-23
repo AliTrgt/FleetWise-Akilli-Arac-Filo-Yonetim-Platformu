ed718586ff1af94f3c39b77c568cc7dd51ade4f1
AliTrgt <alitrgt@outlook.com>
2025-06-23 09:11:29 +0300

    Add method to find Role by name in RoleRepository

---
+++ AuthService/src/main/java/com/example/AuthService/config/DataLoader.java
@@ -0,0 +1,35 @@
+package com.example.AuthService.config;
+
+import com.example.AuthService.entity.Role;
+import com.example.AuthService.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    public DataLoader(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+            createRoleIfNonExists("ADMIN");
+            createRoleIfNonExists("USER");
+            createRoleIfNonExists("VIEWER");
+    }
+
+
+    private void createRoleIfNonExists(String roleName){
+            if (roleRepository.findByRole(roleName).isEmpty()){
+                        Role role = new Role();
+                        role.setRole(roleName);
+                        roleRepository.save(role);
+            }
+    }
+
+
+}
